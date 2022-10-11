708c2b7b544eff82d047c925b779d8bcd94a84c3
tomoya <cj.0s0ttw.nsm4@gmail.com>
2022-10-11 15:28:59 +0900

    Global State追加

--- src/pages/Top.js
+++ src/pages/Top.js
@@ -1,13 +1,22 @@
-import React, { useEffect } from "react";
+import React, { useContext, useEffect } from "react";
 import Layout from "../components/Layout/Layout";
 import { fetchPopularData } from "../apis/index";
+import { Store } from "../store/index";
 
 const Top = () => {
+  const { globalState, setGlobalState } = useContext(Store);
+
   useEffect(() => {
     fetchPopularData().then((res) => {
       console.log(res);
+      setGlobalState({
+        type: "SET_POPULAR",
+        payload: { popular: res.data.items },
+      });
     });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
+
   return <Layout>トップページ</Layout>;
 };
 
