dd54e5cf919bf335578649f6ebdbf7a2a7619e69
tomoya <cj.0s0ttw.nsm4@gmail.com>
2022-10-11 21:00:35 +0900

    video詳細の取得

--- src/components/VideoDetail/VideoDetail.js
+++ src/components/VideoDetail/VideoDetail.js
@@ -1,13 +1,23 @@
-import React, { useEffect } from "react";
+import React, { useContext, useEffect } from "react";
 import { useLocation } from "react-router-dom";
+import { fetchSelectedData } from "../../apis/index";
+import { Store } from "../../store/index";
 
 const VideoDetail = () => {
+  const { globalState, setGlobalState } = useContext(Store);
   const location = useLocation();
-  useEffect(() => {
+  const selectedVideo = async () => {
     const searchParams = new URLSearchParams(location.search);
     const id = searchParams.get("v");
-    console.log(location);
-    console.log(id);
+    await fetchSelectedData(id).then((res) => {
+      const item = res.data.items.shift();
+      setGlobalState({ type: "SET_SELECTED", payload: { selected: item } });
+      console.log(res);
+    });
+  };
+
+  useEffect(() => {
+    selectedVideo();
   }, []);
   return <div>VideoDetail</div>;
 };
