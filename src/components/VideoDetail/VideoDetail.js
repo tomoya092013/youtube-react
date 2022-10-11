22ce6c4889ce6cf71ca133a3be8d9503296a55ca
tomoya <cj.0s0ttw.nsm4@gmail.com>
2022-10-11 21:54:33 +0900

    動画プレーヤー追加

--- src/components/VideoDetail/VideoDetail.js
+++ src/components/VideoDetail/VideoDetail.js
@@ -2,11 +2,14 @@ import React, { useContext, useEffect } from "react";
 import { useLocation } from "react-router-dom";
 import { fetchSelectedData } from "../../apis/index";
 import { Store } from "../../store/index";
+import VideoPlay from "../VideoPlay/VideoPlay";
+import Style from "./VideoDetail.module.scss";
+import Linkify from "react-linkify";
 
 const VideoDetail = () => {
   const { globalState, setGlobalState } = useContext(Store);
   const location = useLocation();
-  const selectedVideo = async () => {
+  const setSelectedVideo = async () => {
     const searchParams = new URLSearchParams(location.search);
     const id = searchParams.get("v");
     await fetchSelectedData(id).then((res) => {
@@ -17,9 +20,21 @@ const VideoDetail = () => {
   };
 
   useEffect(() => {
-    selectedVideo();
+    setSelectedVideo();
   }, []);
-  return <div>VideoDetail</div>;
+
+  return globalState.selected && globalState.selected.id ? (
+    <div className={Style.wrap}>
+      <VideoPlay id={globalState.selected.id} />
+      <p>{globalState.selected.snippet.title}</p>
+      <hr />
+      <Linkify>
+        <pre>{globalState.selected.snippet.description}</pre>
+      </Linkify>
+    </div>
+  ) : (
+    <span>no data</span>
+  );
 };
 
 export default VideoDetail;
